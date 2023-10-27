391d2e5259c971b1da097944e8afb091494f9c07
AsaMitaka <cepelits@gmail.com>
2023-10-27 22:51:46 +0300

    layout wrapper

---
+++ components/layout.tsx
@@ -0,0 +1,20 @@
+import Footer from './footer/footer';
+import Header from './header/header';
+
+interface LayoutProps {
+  children: React.ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ children }) => {
+  return (
+    <div className="h-screen">
+      <Header />
+      <div className="container min-h-full flex flex-col mx-auto max-w-5xl lg:max-w-7xl xl:px-30">
+        <div className="flex mt-5">{children}</div>
+      </div>
+      <Footer />
+    </div>
+  );
+};
+
+export default Layout;

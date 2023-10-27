6af0c16b4e8c69194eb48a8b5691e3541eeb5130
AsaMitaka <cepelits@gmail.com>
2023-10-27 22:52:01 +0300

    header

---
+++ components/header/header.tsx
@@ -0,0 +1,44 @@
+import HeaderItem from './headerItem';
+import HeaderLogo from './headerLogo';
+
+const Header = () => {
+  const items = [
+    {
+      label: 'Films',
+      href: '/',
+      auth: false,
+    },
+    {
+      label: 'tv series',
+      href: '/series',
+      auth: false,
+    },
+    {
+      label: 'profile',
+      href: '/user/1',
+      auth: true,
+    },
+  ];
+
+  return (
+    <div className="w-full sticky text-white bg-neutral-500">
+      <div className="flex py-3 flex-row items-center justify-between mx-auto max-w-5xl lg:max-w-7xl">
+        <div>
+          <HeaderLogo />
+        </div>
+        <div className="flex flex-row items-center gap-5">
+          {items.map((item, index) => (
+            <HeaderItem
+              auth={item.auth}
+              href={item.href}
+              key={`${index}__${item.label}`}
+              label={item.label}
+            />
+          ))}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Header;

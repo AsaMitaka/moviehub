e629f51d0f1670a0fc8d4b4c63b1de0045f5f018
AsaMitaka <cepelits@gmail.com>
2023-10-27 22:51:36 +0300

    footer

---
+++ components/footer/footer.tsx
@@ -0,0 +1,14 @@
+const Footer = () => {
+  return (
+    <div className="w-full sticky text-white bg-neutral-500">
+      <div className="py-3 flex flex-col mx-auto max-w-5xl lg:max-w-7xl">
+        <h1 className="text-white text-lg">Footer</h1>
+        <p className="text-neutral-400 text-xs">
+          © Asa Mitaka. Made by fans. Film data from TMDb. Mobile site.
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default Footer;

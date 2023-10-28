d4ca619aebd4c978c33429c11613e511283eb3e7
AsaMitaka <cepelits@gmail.com>
2023-10-28 20:50:54 +0300

    ui button

--- components/ui/button.tsx
+++ components/ui/button.tsx
@@ -3,10 +3,11 @@ import { useCallback } from 'react';
 interface ButtonProps {
   disabled?: boolean;
   label: string;
+  large?: boolean;
   onClick: () => void;
 }
 
-const Button: React.FC<ButtonProps> = ({ disabled, label, onClick }) => {
+const Button: React.FC<ButtonProps> = ({ disabled, label, large, onClick }) => {
   const handleClick = useCallback(() => {
     if (disabled) {
       return;
@@ -16,7 +17,11 @@ const Button: React.FC<ButtonProps> = ({ disabled, label, onClick }) => {
   }, [disabled, onClick]);
 
   return (
-    <button className="" onClick={handleClick}>
+    <button
+      className={`px-2 py-1 border-1 border-neutral-500 text-white 
+      ${large ? 'w-full' : 'w-fit'}
+      text-lg bg-green-400 cursor-pointer hover:bg-green-600 disabled:bg-green-200`}
+      onClick={handleClick}>
       {label}
     </button>
   );

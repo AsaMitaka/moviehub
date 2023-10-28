cdd626c49bb78d7757ac88c6fd349912661e5e64
AsaMitaka <cepelits@gmail.com>
2023-10-28 20:51:15 +0300

    main app

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,11 +1,17 @@
 import Layout from '@/components/layout';
+import LoginModal from '@/components/modal/loginModal';
+import SignupModal from '@/components/modal/signupModal';
 import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <Layout>
-      <Component {...pageProps} />
-    </Layout>
+    <>
+      <SignupModal />
+      <LoginModal />
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    </>
   );
 }

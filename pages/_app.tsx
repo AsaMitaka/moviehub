51fed7c4ec318fde6a314fb26de22ab50c25e891
AsaMitaka <cepelits@gmail.com>
2023-10-29 23:19:55 +0200

    app page

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,16 +2,17 @@ import Layout from '@/components/layout';
 import LoginModal from '@/components/modal/loginModal';
 import SignupModal from '@/components/modal/signupModal';
 import '@/styles/globals.css';
+import { SessionProvider } from 'next-auth/react';
 import type { AppProps } from 'next/app';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
-    <>
+    <SessionProvider session={pageProps.session}>
       <SignupModal />
       <LoginModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>
-    </>
+    </SessionProvider>
   );
 }

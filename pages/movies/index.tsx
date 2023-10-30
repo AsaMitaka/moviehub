be77539fa4cc54c32f6c28f4df38639ef98c04cb
AsaMitaka <cepelits@gmail.com>
2023-10-31 00:53:22 +0200

    movie page

--- pages/movies/index.tsx
+++ pages/movies/index.tsx
@@ -1,22 +1,26 @@
+import ContainerButton from '@/components/containerButton';
 import FilmContainer from '@/components/film/filmContainter';
 import useMovies from '@/hooks/useMovies';
+import { useState } from 'react';
 import { ClipLoader } from 'react-spinners';
 
 const Movies = () => {
-  const { data: fetchedData, isLoading } = useMovies();
+  const [page, setPage] = useState(1);
+  const { data: fetchedData, isLoading } = useMovies(page as number);
   console.log(fetchedData);
 
   if (isLoading) {
     return (
-      <div className="flex flex-col items-center justify-center">
+      <div className="w-screen px-3 py-4 flex items-center justify-center">
         <ClipLoader color="text-green-500" size={80} />
       </div>
     );
   }
 
   return (
-    <div>
-      <FilmContainer data={fetchedData.results} page={fetchedData.page} />
+    <div className="px-2 py-3">
+      <FilmContainer data={fetchedData.results} />
+      <ContainerButton page={page} setPage={setPage} />
     </div>
   );
 };

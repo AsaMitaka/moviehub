4c6c09ea879f807e2f4f714c6f181948a6da4150
AsaMitaka <cepelits@gmail.com>
2023-10-31 00:53:37 +0200

    serie page

--- pages/series/index.tsx
+++ pages/series/index.tsx
@@ -1,14 +1,17 @@
+import ContainerButton from '@/components/containerButton';
 import SeriesContainer from '@/components/series/seriesContainer';
 import useSeries from '@/hooks/useSeries';
+
+import { useState } from 'react';
 import { ClipLoader } from 'react-spinners';
 
 const Series = () => {
-  const { data: fetchedData, isLoading } = useSeries();
-  console.log(fetchedData);
+  const [page, setPage] = useState(1);
+  const { data: fetchedData, isLoading } = useSeries(page as number);
 
   if (isLoading) {
     return (
-      <div className="flex flex-col items-center justify-center">
+      <div className="w-screen px-3 py-4 flex items-center justify-center">
         <ClipLoader color="text-green-500" size={80} />
       </div>
     );
@@ -16,7 +19,8 @@ const Series = () => {
 
   return (
     <div className="px-2 py-3">
-      <SeriesContainer data={fetchedData.results} page={fetchedData.page} />
+      <SeriesContainer data={fetchedData.results} />
+      <ContainerButton page={page} setPage={setPage} />
     </div>
   );
 };

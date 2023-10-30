0da8202bf656aa9df1907c9240a1296401020598
AsaMitaka <cepelits@gmail.com>
2023-10-31 00:52:53 +0200

    ratingBlock

---
+++ components/ratingBlock/ratingBlock.tsx
@@ -0,0 +1,44 @@
+import {
+  AiOutlineEye,
+  AiFillEye,
+  AiFillHeart,
+  AiOutlineHeart,
+  AiOutlineStar,
+  AiFillStar,
+} from 'react-icons/ai';
+
+interface RatingBlockProps {
+  isLiked?: boolean;
+  isWatched?: boolean;
+  rating?: number;
+}
+
+const RatingBlock: React.FC<RatingBlockProps> = ({ isLiked, isWatched, rating }) => {
+  return (
+    <div className="flex flex-col w-[250px] rounded-md gap-2 bg-neutral-500">
+      <div className="flex flex-row items-center justify-around px-3 py-2 ">
+        <div className="flex flex-col items-center  cursor-pointer hover:opacity-30">
+          <AiOutlineEye color="white" size={28} />
+          <p>Watch</p>
+        </div>
+        <div className="flex flex-col cursor-pointer hover:opacity-30">
+          <AiOutlineHeart color="white" size={28} />
+          <p>Like</p>
+        </div>
+      </div>
+      <hr />
+      <div className="flex flex-col items-center gap-2 px-3 py-2">
+        <p className="text-xl text-neutral-100">Rate</p>
+        <div className="flex flex-row items-center gap-1">
+          <AiOutlineStar className="cursor-pointer hover:opacity-70" size={24} />
+          <AiOutlineStar className="cursor-pointer hover:opacity-70" size={24} />
+          <AiOutlineStar className="cursor-pointer hover:opacity-70" size={24} />
+          <AiOutlineStar className="cursor-pointer hover:opacity-70" size={24} />
+          <AiOutlineStar className="cursor-pointer hover:opacity-70" size={24} />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default RatingBlock;

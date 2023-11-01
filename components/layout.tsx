fe102dca2250f60622981af33346acddc8335ea7
AsaMitaka <cepelits@gmail.com>
2023-11-01 02:20:12 +0200

    layout minor fix

--- components/layout.tsx
+++ components/layout.tsx
@@ -10,7 +10,7 @@ const Layout: React.FC<LayoutProps> = ({ children }) => {
     <div className="h-screen">
       <Header />
       <div className="container min-h-full flex flex-col mx-auto max-w-5xl lg:max-w-7xl xl:px-30">
-        <div className="flex mt-5">{children}</div>
+        <div className="flex h-full mt-5">{children}</div>
       </div>
       <Footer />
     </div>

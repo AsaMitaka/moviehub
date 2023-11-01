c4b7362baf1f61604498a4a7d3605fb8fcf1db82
AsaMitaka <cepelits@gmail.com>
2023-11-02 00:15:58 +0200

    app page minor fix

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -6,12 +6,14 @@ import '@/styles/globals.css';
 import Layout from '@/components/layout';
 import LoginModal from '@/components/modal/loginModal';
 import SignupModal from '@/components/modal/signupModal';
+import EditModal from '@/components/modal/editModal';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <SessionProvider session={pageProps.session}>
-      <SignupModal />
+      <EditModal />
       <LoginModal />
+      <SignupModal />
       <Layout>
         <Component {...pageProps} />
       </Layout>

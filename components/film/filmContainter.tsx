58565caa890289ee4811050dff3686ce1082eb91
AsaMitaka <cepelits@gmail.com>
2023-11-01 02:20:33 +0200

    film component

--- components/film/filmContainter.tsx
+++ components/film/filmContainter.tsx
@@ -6,7 +6,7 @@ interface FilmContainerProps {
 
 const FilmContainer: React.FC<FilmContainerProps> = ({ data }) => {
   return (
-    <div className="mb-4 grid grid-cols-10 grid-rows-2 gap-4">
+    <div className="mb-4 grid xl:grid-cols-8 xl:grid-rows-3 lg:grid-cols-6 lg:grid-rows-4 md:grid-cols-4 md:grid-rows-5 sm:grid-cols-2 sm:grim-rows-10 gap-4">
       {data &&
         data.map((data: Record<string, any>, index: number) => (
           <FilmItem data={data} key={index} />
